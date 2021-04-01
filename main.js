ea69f4cd016d309669a172fc7cbe7b9189b53a15
bensalemd <bensalem.dja@gmail.com>
2021-04-02 00:31:48 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+fetch('./holiday2020.json').then(async (resp) => {
+    const holidays2020 = await resp.json();
+    const currentDate = new Date();
+    currentDate.setHours(0,0,0,0);
+    const holiday = holidays2020?.find((el) => el.date === currentDate.toISOString());
+
+    document.getElementById('holiday').innerText = holiday ? `Yes\nIt is ${holiday.name}` : 'No';
+})
